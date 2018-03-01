f333b31250508b355a1abf741d7c44a220580179
Sara Helba <sara.helba@tekworx.us>
2018-03-01 14:57:50 -0500

    created rough (some hardcoded vals) version of modern gauge

--- gauge.js
+++ gauge.js
@@ -1,8 +1,8 @@
 var kW = {value: 1625, range: [0, 7000], units: 'kW', title: ['Plant', 'kW']}
 var tR = {value: 3000, range: [0, 6500], units: 'tR', title: ['Plant', 'tR']}
-var kW_tR = {value: Number((kW.value / tR.value).toString().slice(0, 4)), range: [1, 0], units: 'kW/tR', title: ['System', 'Efficiency']}    //remember to invert range
+var kW_tR = {value: Number((kW.value / tR.value).toString().slice(0, 4)), range: [2, 0], units: 'kW/tR', title: ['System', 'Efficiency']}    //remember to invert range
 
-kW_tR.value = 0.3
+// kW_tR.value = 0.68
 
 var selectedMeasurement = kW_tR // based on gauge we're creating
 
@@ -11,6 +11,7 @@ var {units} = selectedMeasurement
 
 const backgroundColors = ['#FFC4C2', '#F7F1C3', '#C5F0DC']
 const foregroundColors = ['#21A75D', '#ffd829', '#c01616']
+const gray = '#474747'
 
 var cx = 50;
 var cy = 50;
@@ -19,10 +20,10 @@ var endAngle = Math.PI || Math.PI - .9;
 var radiansPerWedge = (endAngle - startAngle) / 3;
 var innerRadius = 65
 var outerRadius = 90
-var margin = {left: 500, right: 500, top: 400, bottom: 0}
+var margin = {left: 200, right: 200, top: 200, bottom: 0}
 
 const colorScale = d3.scaleQuantize()   
-    .domain(selectedMeasurement.range)
+    .domain([selectedMeasurement.range[1], selectedMeasurement.range[0]])
     .range(foregroundColors)
 
 const angleScale = d3.scaleLinear()
@@ -40,12 +41,12 @@ const svg = d3.select('body').append('svg')
 const graphicGroup = svg.append('g').attr('class', 'graphicGroup').attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 const borderCircle = graphicGroup.append('circle')
-    .attr('class', 'borderCircle')
+    .attr('id', 'borderCircle')
     .attr('cx', cx)
     .attr('cy', cy)
     .attr('r', 100)
     .attr('fill', 'none')
-    .attr('stroke', '#474747')
+    .attr('stroke', gray)
     .attr('stroke-width', '7px')
 
 const chartGroup = graphicGroup.append('g').attr('class', 'chartGroup').attr('transform', `translate(${cx}, ${cy})`)
@@ -55,6 +56,7 @@ const gaugeArcGenerator = d3.arc()
     .endAngle(angleScale(selectedMeasurement.value))
     .innerRadius(innerRadius)
     .outerRadius(outerRadius)
+    .cornerRadius('50')
 
 const threeWedgeArcsGenerator = d3.arc() 
     .startAngle((d, i) => startAngle + (radiansPerWedge * i))
@@ -62,11 +64,22 @@ const threeWedgeArcsGenerator = d3.arc()
     .innerRadius(innerRadius)
     .outerRadius(outerRadius)
 
-const foregroundArcsGenerator = d3.arc()
-    .startAngle((d, i) => startAngle + (radiansPerWedge * i))   
-    .endAngle((d, i) => startAngle + (radiansPerWedge * i) + d)   //this wedge's start angle plus d
+const titleArcGenerator = d3.arc()
+    .startAngle(startAngle)
+    .endAngle(endAngle)
     .innerRadius(innerRadius)
-    .outerRadius(outerRadius)
+    .outerRadius(outerRadius + 20)
+
+const unitsArcGenerator = d3.arc()
+    .startAngle(endAngle)
+    .endAngle(startAngle)
+    .innerRadius(innerRadius - 2)
+    .outerRadius(innerRadius - 2)
+// const foregroundArcsGenerator = d3.arc()
+//     .startAngle((d, i) => startAngle + (radiansPerWedge * i))   
+//     .endAngle((d, i) => startAngle + (radiansPerWedge * i) + d)   //this wedge's start angle plus d
+//     .innerRadius(innerRadius)
+//     .outerRadius(outerRadius)
 
 
 
@@ -101,32 +114,60 @@ if (leftoverRadians > 0) foregroundWedgeFillData.push(leftoverRadians)
 //             .attr('fill', (d, i) => foregroundColors[i])
 
 const gauge = chartGroup.append('path')
-    .attr('class', 'gauge')
+    .attr('id', 'gauge')
     .attr('d', gaugeArcGenerator())
     // .attr('stroke', 'gray')
     .attr('fill', colorScale(selectedMeasurement.value))
     
 
-const textGroup = chartGroup.append('g')
-    .attr('class', 'textGroup')
+// const textGroup = chartGroup.append('g')
+//     .attr('class', 'textGroup')
 
-textGroup.append('text').selectAll('tspan')
-    .data(selectedMeasurement.title)
-    .enter().append('tspan')
-        .attr('x', 0)
-        .attr('y', (d, i) => (i * 30) - 27)
-        .attr('class', (d, i) => `title-${i + 1}`)
-        .attr('text-anchor', 'middle')
-        .attr('font-size', '1.3em')
-        .attr('fill', '#75757a')
-        .text(d => d)
+// textGroup.append('text').selectAll('tspan')
+//     .data(selectedMeasurement.title)
+//     .enter().append('tspan')
+//         .attr('x', 0)
+//         .attr('y', (d, i) => (i * 30) - 27)
+//         .attr('class', (d, i) => `title-${i + 1}`)
+//         .attr('text-anchor', 'middle')
+//         .attr('font-size', '1.3em')
+//         .attr('fill', '#75757a')
+//         .text(d => d)
 
-const valueOutput = textGroup.append('text')
+const valueOutput = chartGroup.append('text')
     .attr('class', 'valueOutput')
     .attr('x', 0)
-    .attr('y', cy - 5)
+    .attr('y', cy - 40)
     .attr('text-anchor', 'middle')
-    .attr('font-size', '1.05em')
+    .attr('font-size', '3.05em')
     .attr('font-weight', 'bold')
-    .text(`${selectedMeasurement.value} ${selectedMeasurement.units}`)
+    .text(selectedMeasurement.value)
 
+
+
+const textPath = chartGroup.append('path')
+    .attr('id', 'textPath')
+    .attr('d', titleArcGenerator())
+    .attr('fill', 'none')
+
+
+const textPath2 = chartGroup.append('path')
+    .attr('id', 'textPath2')
+    .attr('d', unitsArcGenerator())
+    .attr('fill', 'none')
+
+const title = chartGroup.append("text").append("textPath")
+    .attr("xlink:href", "#textPath") //ID of path
+    .style("text-anchor","middle")
+    .attr("startOffset", "25%")
+    .attr('font-size', '1.3em')
+    .attr('fill', '#75757a')
+    .text(selectedMeasurement.title.join(' '));
+
+const unitsOutput = chartGroup.append("text").append("textPath")
+    .attr("xlink:href", "#textPath2") //ID of path
+    .style("text-anchor","end")
+    .attr("startOffset", "50%")
+    .attr('font-size', '1.3em')
+    .attr('fill', '#75757a')
+    .text(selectedMeasurement.units);
